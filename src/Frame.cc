d053c37c7b38497100474976ec39346ebbe8e35b
Blake W. Ford <blakewford@gmail.com>
2019-11-30 16:58:10 -0600

    Debug improvements.

--- src/Frame.cc
+++ src/Frame.cc
@@ -75,10 +75,13 @@ Frame::Frame(const cv::Mat &imLeft, const cv::Mat &imRight, const double &timeSt
     mvInvLevelSigma2 = mpORBextractorLeft->GetInverseScaleSigmaSquares();
 
     // ORB extraction
-    thread threadLeft(&Frame::ExtractORB,this,0,imLeft);
-    thread threadRight(&Frame::ExtractORB,this,1,imRight);
-    threadLeft.join();
-    threadRight.join();
+//    thread threadLeft(&Frame::ExtractORB,this,0,imLeft);
+//    thread threadRight(&Frame::ExtractORB,this,1,imRight);
+//    threadLeft.join();
+//    threadRight.join();
+
+    ExtractORB(0,imLeft);
+    ExtractORB(1,imRight);
 
     N = mvKeys.size();
 
